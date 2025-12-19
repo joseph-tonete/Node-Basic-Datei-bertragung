5e48167b7cd9ab989835397afc36ebe31ce4872b
joseph.knoener <joseph.knoener@unasp.edu.br>
2025-12-19 15:58:02 -0300

    Post route /upload-file

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ const port = 1000
 
 const fs = require('fs')
 
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "files/")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage})
+
 app.use(express.json())
+
+app.get('/images', (req, res) => {
+    
+})
 
 app.get('/', (req, res) => {
     fs.readFile('public/index.html', 'utf-8', (err,data) => {
@@ -16,7 +31,8 @@ app.get('/', (req, res) => {
     })
 })
 
-app.post('/upload-file', (req, res) => {
+app.post('/upload-file', upload.single("file"), (req, res) => {
+    console.log(req.file.originalname)
     console.log(req.body)
     res.status(201).json({message: 'Received request!', user: req.body})
 })
